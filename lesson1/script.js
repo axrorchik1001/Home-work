8ab8e19832da684ba2c0f771b6d797881f97fbac
axrordev <abduxolikov.axror@gmail.com>
2022-06-05 23:01:21 +0500

    complete task

---
+++ lesson1/script.js
@@ -0,0 +1,10 @@
+let summ = prompt("Summ kiriting")*1
+
+const dollor = 10654;
+const yevro = 12674;
+
+let summDollor = summ/dollor;
+let summYevro = summ/yevro;
+
+alert(`${Math.floor(summDollor)}  dollor  yoki  ${summDollor} dollor 
+\n ${Math.floor(summYevro)}  yevro  yoki  ${summYevro} yevro `)
