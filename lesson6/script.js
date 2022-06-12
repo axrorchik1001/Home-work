210c632da5fb80539f085f1f15e1c1654e8447c8
axrordev <abduxolikov.axror@gmail.com>
2022-06-12 08:36:47 +0500

    changed

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,11 +1,22 @@
-let liElement = document.querySelectorAll("li")
+let ulElement = document.querySelector("ul")
+let links = ["Home", "About", "Contact", "Services"]
 
-liElement.forEach(function(el){
-    el.addEventListener("click", function(){
-        liElement.forEach(function(e){
-            e.classList = ""
-        })
-        el.classList.add("active")
+links.forEach(function(el){
+    let liElement = document.createElement("li")
+    liElement.textContent = el
+    ulElement.appendChild(liElement)
+
+    
+})
+
+let navLinks = document.querySelectorAll("li")
+
+navLinks.forEach(function(link){
+    link.addEventListener("click", function(){
+            navLinks.forEach(function(e){
+                e.classList = ""
+            })
+        link.classList.add("active")
         
     })
 })
